53946733068cc7d4ec88be16abb919755a8f9659
Jairo Hernández <55113454+LordGew@users.noreply.github.com>
2025-04-10 12:33:37 -0400

    a

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+    <meta name="google-site-verification" content="oUQIPkn93FDoqCdfB4dKqeN09x1jeLwzXz8D91hMFu0" /><meta name="google-site-verification" content="oUQIPkn93FDoqCdfB4dKqeN09x1jeLwzXz8D91hMFu0" />
       <title> EntropiuX wow</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Disfruta de una experiencia única en WoW: War Within en nuestro servidor privado. Con características avanzadas, seguridad total y una comunidad activa, ¡juega gratis y descubre todo lo que Azeroth tiene para ofrecer!" />
